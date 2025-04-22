8c32c78a3d929533e2c3e816781630df1c43f7c9
emcahell <emmanuelltowers@gmail.com>
2025-04-21 23:54:45 -0400

    add styles footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,29 @@
 import React from "react";
+import Icongh from "./icons/Icongh";
+import IconX from "./icons/IconX";
+import Iconig from "./icons/Iconig";
 
 const Footer = () => {
   return (
-    <footer className="absolute bottom-0 flex justify-center items-center w-full h-20 bg-wine-dark">
+    <footer className="absolute bottom-0 flex flex-col justify-center items-center w-full py-14 border-t-2 border-wine bg-wine-dark">
+
+      <section className="flex flex-col items-center justify-center tracking-wide text-center mb-5 text-white-smoke">
+        <p className="text-sm">Desarrollado con ❤️ por <b>Emcahell</b></p>
+        <div className="flex justify-center items-center gap-4 mt-4">
+          <a className="m-2 p-2 rounded-full bg-wine-light hover:bg-wine" href="https://github.com/emcahell">
+            <Icongh />
+          </a>
+          <a className="m-2 p-2 rounded-full bg-wine-light hover:bg-wine" href="https://x.com/kromwellykz">
+            <IconX />
+          </a>
+          <a className="m-2 p-2 rounded-full bg-wine-light hover:bg-wine" href="https://instagram.com/kromwellykz">
+            <Iconig />
+          </a>
+        </div>
+      </section>
+      <section className="text-white-smoke mt-4 text-sm">
+        <p>© 2025 Emcahell - Estados vzla</p>
+      </section>
 
     </footer>
   )
