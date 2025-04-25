b605ba74c49e1398f9330689c4de7abdc7e868f9
emcahell <emmanuelltowers@gmail.com>
2025-04-25 18:08:10 -0400

    create page search and add state images, also complete the data

---
+++ src/app/Search.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import InputSearch from "../components/ui/InputSearch";
+
+const Search = () => {
+  return (
+    <section>
+      <article>
+        <h1>Busca tu estado</h1>
+        <div>
+          <InputSearch />
+        </div>
+      </article>
+      <article></article>
+    </section>
+  )
+}
+
+export default Search;
---
+++ src/components/ui/InputSearch.jsx
@@ -0,0 +1,9 @@
+const InputSearch = ({ placeholder, value, onChange }) => {
+  return (
+    <>
+      <input type="search" name="search" id="" />
+    </>
+  )
+}
+
+export default InputSearch;
