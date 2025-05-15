2f3f6ea9ef7a78d07d19c13e2f05e97c9d7376a1
emcahell <emmanuelltowers@gmail.com>
2025-05-14 22:00:39 -0400

    prueva images

--- src/components/images/Mapa.jsx
+++ src/components/images/Mapa.jsx
@@ -2,7 +2,7 @@ const Mapa = () => {
   return (
     <>
     <img 
-    src="./src/assets/images/mapa-vzla.png" 
+    src="../assets/images/mapa-vzla.png" 
     alt="mapa venezuela" 
     className="relative -top-38 drop-shadow-2xl"
     />
